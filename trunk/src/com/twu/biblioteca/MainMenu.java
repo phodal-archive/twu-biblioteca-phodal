fcd80cd46475a65288af8507370ca8c07f0d3870
gmszone@gmail.com <gmszone@gmail.com@cd71c66a-8e2a-b832-6e46-115d0ac06b7d>
2014-09-09 09:57:45 +0000

    add simple input method

---
+++ trunk/src/com/twu/biblioteca/MainMenu.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by fdhuang on 9/9/14.
+ */
+public class MainMenu {
+    public List<String> allMenu() {
+        return new ArrayList<>();
+    }
+}
