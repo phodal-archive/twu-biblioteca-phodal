a5589ffd1b641c3653548c546d976cd83750b70c
gmszone@gmail.com <gmszone@gmail.com@cd71c66a-8e2a-b832-6e46-115d0ac06b7d>
2014-09-09 05:47:42 +0000

    user can list books

--- trunk/src/com/twu/biblioteca/BibliotecaApp.java
+++ trunk/src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,21 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    static Library library = new Library();
 
     public static void main(String[] args) {
-        System.out.println(welcome());
+        System.out.println(Welcome());
+        System.out.println("Hi, There is All Books:");
+        System.out.println(ListAllBooks());
     }
 
-    public static String welcome(){
+    public static String Welcome(){
         return "Welcome to Biblioteca Library";
     }
+
+    public static String ListAllBooks(){
+        return library.getAllBookName();
+    }
+
 }
 
