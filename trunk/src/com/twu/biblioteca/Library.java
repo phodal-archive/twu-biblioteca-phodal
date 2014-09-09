9c3bb031d540897b042cb98c9f09968225cc7ede
gmszone@gmail.com <gmszone@gmail.com@cd71c66a-8e2a-b832-6e46-115d0ac06b7d>
2014-09-09 09:42:34 +0000

    add books list

--- trunk/src/com/twu/biblioteca/Library.java
+++ trunk/src/com/twu/biblioteca/Library.java
@@ -1,14 +1,28 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by fdhuang on 9/9/14.
  */
 
 class Library {
-    Books book = new Books("Design IOT", "Phodal", "2014");
+    private Books book = new Books("Design IOT", "Phodal", "2014");
+    private List<Books> allBooks = new ArrayList<>();
 
+    public Library(){
+        allBooks.add(book);
+    }
     public String getAllBookName() {
-        return book.getName();
+        String results = "";
+        for(int i=0;i<allBooks.size();i++) {
+            results += allBooks.get(i).getName();
+        }
+        return results;
     }
 
+    public String getAllBookDetails() {
+        return allBooks.get(0).getAuthor().toString();
+    }
 }
