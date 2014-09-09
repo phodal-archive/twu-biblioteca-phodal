07c180695bc14f099505ce0d9f0d81d817e2441b
gmszone@gmail.com <gmszone@gmail.com@cd71c66a-8e2a-b832-6e46-115d0ac06b7d>
2014-09-09 04:02:42 +0000

    Add the welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(welcome());
+    }
+
+    public static String welcome(){
+        return "Welcome to Biblioteca Library";
     }
 }
+
