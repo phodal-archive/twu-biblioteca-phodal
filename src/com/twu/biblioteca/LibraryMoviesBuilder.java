7bd898f6ba1829cb05bd0bec677565692df61025
gmszone@gmail.com <gmszone@gmail.com@cd71c66a-8e2a-b832-6e46-115d0ac06b7d>
2014-09-15 10:06:19 +0000

    [LIST MOVIES] add movie builder

---
+++ src/com/twu/biblioteca/LibraryMoviesBuilder.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by fdhuang on 9/15/14.
+ */
+public class LibraryMoviesBuilder {
+    private Movies movie;
+    private String name;
+    private int year;
+    private String director;
+    private int rating;
+
+    public LibraryMoviesBuilder addName(Movies movie) {
+        this.name = movie.getName();
+        return this;
+    }
+
+    public LibraryMoviesBuilder addYear(Movies movie) {
+        this.year = movie.getYear();
+        return this;
+    }
+
+    public LibraryMoviesBuilder addDirector(Movies movie) {
+        this.director = movie.getDirector();
+        return this;
+    }
+
+    public LibraryMoviesBuilder addRating(Movies movie) {
+        this.rating = movie.getRating();
+        return this;
+    }
+
+    public String build() {
+        return this.name + "," +
+                this.year + "," +
+                this.director + "," +
+                this.rating + "\n";
+    }
+}
