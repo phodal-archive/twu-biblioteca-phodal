f141ea775468b87800ea9e860dbd3f517adbcd5c
gmszone@gmail.com <gmszone@gmail.com@cd71c66a-8e2a-b832-6e46-115d0ac06b7d>
2014-09-15 15:33:14 +0000

    [Check-out Movie] : do some refactor

--- src/com/twu/biblioteca/LibraryBook.java
+++ src/com/twu/biblioteca/LibraryBook.java
@@ -4,23 +4,9 @@ package com.twu.biblioteca;
  * Created by fdhuang on 9/10/14.
  */
 public class LibraryBook extends Book {
-    private Boolean isBookInLibrary;
 
     public LibraryBook(String bookName, String author, int publishDate) {
         super(bookName, author, publishDate);
-        this.isBookInLibrary = true;
-    }
-
-    public void setBookCheckout() {
-        this.isBookInLibrary = false;
-    }
-
-    public void setBookReturn() {
-        this.isBookInLibrary = true;
-    }
-
-    public Boolean isBookInLibrary() {
-        return isBookInLibrary;
     }
 }
 
