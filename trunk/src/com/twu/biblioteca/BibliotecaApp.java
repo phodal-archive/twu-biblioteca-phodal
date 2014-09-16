8639b58dcc00e6999d2193b7f5f190b79fc46b7b
gmszone@gmail.com <gmszone@gmail.com@cd71c66a-8e2a-b832-6e46-115d0ac06b7d>
2014-09-16 01:23:51 +0000

    [Check-out Movie] : add to the library app

--- trunk/src/com/twu/biblioteca/BibliotecaApp.java
+++ trunk/src/com/twu/biblioteca/BibliotecaApp.java
@@ -7,9 +7,13 @@ import java.util.Scanner;
 
 public class BibliotecaApp {
     private static Book book = new Book("Design IOT", "Phodal", 2014);
-    private static Book book2 = new Book("Design IOT 2", "Phodal", 2016);
+    private static Book book2 = new Book("Design IOT 2", "Phodal", 2014);
+    private static List<Movies> movies = new ArrayList<>();
+    private static Movies movie1 = new Movies("design patterns",1994,"GOF",10);
+    private static Movies movie2 = new Movies("design patterns 2",1996,"GOF",10);
     private static List<Book> allBooks = new ArrayList<>();
-    private static Library library = new Library(allBooks);
+    private static Library library = new Library(allBooks, movies);
+
     private static String welcomeMessage = "Welcome to Biblioteca Library";
 
     public static void main(String[] args) throws IOException {
@@ -17,6 +21,7 @@ public class BibliotecaApp {
         System.out.println(welcomeMessage);
         System.out.println("Choose you Menu, Enter the Number to Continue:");
         System.out.println("1. List All Books");
+        System.out.println("2. List All Books & Movies");
 
         String key = "";
         while(key.equals("Quit") != true) {
@@ -24,19 +29,24 @@ public class BibliotecaApp {
             key = sc.nextLine();
 
             System.out.println("You Select Menu: " + key);
-            System.out.println(HandlerMenuInput(key));
+            String results = HandlerMenuInput(key);
+            System.out.println(results);
         }
     }
 
     private static void initLibrary() {
         allBooks.add(book);
         allBooks.add(book2);
+        movies.add(movie1);
+        movies.add(movie2);
     }
 
     private static String HandlerMenuInput(String key) {
         switch (key) {
             case "1":
                 return library.getAllBookDetails();
+            case "2":
+                return library.getAllBookDetails() + library.getAllMovies();
             case "Quit":
                 return "";
             default:
